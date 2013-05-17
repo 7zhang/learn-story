2b1aeabac9fd2fe91e1c5af84703e803eaf1adcf
root <root@localhost.localdomain>
2013-05-17 11:27:20 +0800

    nanjing light rain

--- collisiondetection/collisiondetection.cpp
+++ collisiondetection/collisiondetection.cpp
@@ -1,9 +1,15 @@
+#include "collisiondetection.h"
 #include <unistd.h>
 #include <fcntl.h>
 
 int main(void)
 {
-
+	double *stl;
+	stl = stlload("robot_stl/gun.STL");
 }
 
+
+double *stlload(char *path)
+{
+	
 
